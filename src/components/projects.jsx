e2fb5236753dda010b82066127815651d76df72a
Milan Singh <100085851+Mil9nn@users.noreply.github.com>
2025-01-26 20:31:49 +0530

    Trying to fix the problem of images not appearing

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,9 @@
+import omnifoodImage from './../../public/assets/omnifood.png';
+import keeperImage from './../../public/assets/keeper.png';
+
 const projects = [
-    { title: "Omnifood", img: "src/assets/omnifood.png", description: "Omnifood is a visually appealing food website designed to showcase delicious recipes, a food image gallery, and customer testimonials.Offers a smooth user experience on both desktop and mobile devices.", alt: "omnifood-website-pic" },
-    { title: "Keeper", img: "src/assets/keeper.png", description: "The Keeper App is a simple note-making application built with React. It allows users to create and delete notes to help manage their tasks and ideas.", alt: "keeper-app-pic"},
-    { title: "Calculator", img: "src/assets/calculator.png", description: "", alt: "calculator-project-pic"}
+    { title: "Omnifood", img: omnifoodImage, description: "Omnifood is a visually appealing food website designed to showcase delicious recipes, a food image gallery, and customer testimonials.Offers a smooth user experience on both desktop and mobile devices.", alt: "omnifood-website-pic" },
+    { title: "Keeper", img: keeperImage, description: "The Keeper App is a simple note-making application built with React. It allows users to create and delete notes to help manage their tasks and ideas.", alt: "keeper-app-pic"},
   ];
 
 export default projects;
